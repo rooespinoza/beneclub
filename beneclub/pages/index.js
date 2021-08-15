6b7006de9666dc204d20328c6c873a93cadd1993
gonzachristol <gonzalochristol@gmail.com>
2021-08-15 18:35:13 -0300

    fix

--- beneclub/pages/index.js
+++ beneclub/pages/index.js
@@ -1,11 +1,12 @@
 import Image from 'next/image'
-import { Fragment } from 'react'
+import React, { Fragment } from 'react'
 import styles from './../pages/Home/home.module.scss'
 
-export default function Home() {
+const Home = () => {
   return (
     <div className={styles.container}>
     sdfds
     </div>
   )
 }
+export default Home;
