caba110d431c88c03605548086088c8cb0c5019a
rooespinoza <rocio.espinoza@alumnos.frm.utn.edu.ar>
2021-08-15 21:59:43 -0300

    Add title and sumate

--- beneclub/pages/index.js
+++ beneclub/pages/index.js
@@ -4,9 +4,20 @@ import styles from './../pages/Home/home.module.scss'
 
 const Home = () => {
   return (
-    <div className={styles.container}>
-    sdfds
-    </div>
+    <Fragment>
+      <div className={styles.title__container}>
+        <div className={styles.title}><h1>Los mejores beneficios, en un solo lugar</h1></div>
+      </div>
+      <div className={styles.beneficios__container}>
+        beneficios
+      </div>
+      <div className={styles.sumate__container}>
+        <div className={styles.sumate__text}>
+          <h2>Sumate a Andes Salud</h2>
+          Te brindamos salud y motivos para estar saludable.
+        </div>        
+      </div>
+    </Fragment>
   )
 }
 export default Home;
