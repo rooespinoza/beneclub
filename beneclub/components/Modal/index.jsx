c47b8e72c0c44618cd5b8a093cafdb52f18bd03e
rooespinoza <rocio.espinoza@alumnos.frm.utn.edu.ar>
2021-08-27 00:15:45 -0300

    Merge

--- beneclub/components/Modal/index.jsx
+++ beneclub/components/Modal/index.jsx
@@ -4,9 +4,6 @@ import Image from 'next/image';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
-import BeneficioCard from './../BeneficioCard/index';
-import image from 'next/image';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 const ModalComponent = ({ open, toogleModal, beneficio }) => {
     const [width,setWidht]=useState();
